7480af27704b16cece30d47929a80111915c1c11
badboy13s <45422794+suryabb13@users.noreply.github.com>
2024-01-28 12:56:07 +0530

    Update profile2.php

--- profile2.php
+++ profile2.php
@@ -1,17 +1,10 @@
 <?php
-// Connect to Redis
-
 
 $redis = new Redis();
 $redis->connect('127.0.0.1', 6379);
-
 // Get key from GET data
 $sessionId = $_POST['key'];
-
-
-// Retrieve email from Redis using the key
 $email = $redis->get($sessionId);
-
 // Return the email to the client
 echo $email;
 ?>
